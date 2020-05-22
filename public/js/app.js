28b315a7e8a5c0192e28d0a408a581bba5363b5f
Rohit Bairagi <rbairagi165@gmail.com>
2020-05-22 17:40:21 +0530

    remove

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+// consol.log("heyy");
+// const require= 
+
+
+
+const w_form = document.querySelector('form');
+const search= document.querySelector('input');
+const msg1= document.querySelector('#message-1')
+const msg2= document.querySelector('#message-2')
+
+w_form.addEventListener('submit',(e)=>
+{
+    e.preventDefault()
+    const location = search.value
+    console.log(location)
+    fetch('http://localhost:3000/info?search='+location).then((response)=>
+{
+   response.json().then((data)=>
+   {
+       if(data.error){
+        msg2.textContent=data.error;
+       }
+       else{
+           console.log("temp id"+data.temperature)
+           msg2.textContent=data.temperature
+       }
+   })
+})
+})
